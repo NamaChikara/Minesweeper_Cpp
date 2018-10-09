2170bf5f2c6e53b7d4b066679c9e857c096f64a7
Zachary <zackbarry13@gmail.com>
2018-10-09 12:48:47 -0700

    Note: Cell.h and Cell.cpp no longer needed -- use GraphicCell files.

--- Cell.h
+++ Cell.h
@@ -13,6 +13,7 @@ public:
 	bool marked;	// true if the user right clicks the Cell
 	int touching;	// number of bombs a Cell touches
 	bool covered;	// true if Cell has never been clicked on
+	bool mistake;	// true if user left clicks a bomb
 	Cell();
 	Cell(int, int, bool);	// include coordinate and bomb status
 	void action(char);	// respond to left or right click
