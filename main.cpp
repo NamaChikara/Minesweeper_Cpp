31079ab1601cc5124d4b8c9df1795af45078742d
Zachary <zackbarry13@gmail.com>
2018-10-12 09:40:06 -0700

    Updated to place a ColorBar on the sf::RenderWindow

--- main.cpp
+++ main.cpp
@@ -12,38 +12,43 @@
 #include "GraphicCell.h"
 #include "User_Input.h"
 #include "InfoBar.h"
+#include "ColorBar.h"
 
 int main()
 {	
+	//////////////////////////////////////////////////////////////////
+	// Initialize the sf::RenderWindow, Board, InfoBar, and ColorBar
+	//////////////////////////////////////////////////////////////////
+
 	// game specifications
 	int dim = 15;		// number of GraphicCells per row/column (square game board)
 	int bombs = 25;		// number of bombs the board should have
+	std::string font_file = "SourceSansPro.otf";	// font for Text objects
 
-	// initialize info_text_height before InfoBar since needed for RenderWindow dimension
-	float info_text_height = 75;
+	// initialize ColorBar and InfoBar heights since needed for RenderWindow dimension
+	float color_bar_height = 50;
+	float info_bar_height = 50;
 
 	// set RenderWindow dimensions and initialize;
-	int win_height = 800;
-	int win_width = win_height - info_text_height;
+	float win_height = 800;
+	float win_width = win_height - info_bar_height - color_bar_height;
 	sf::RenderWindow window(sf::VideoMode(win_width, win_height), "Minesweeper");
+
+	// initialize ColorBar
+	ColorBar m_color{ 11,win_width,color_bar_height,font_file };
 
 	// y offset for the info bar
 	float info_text_yset = 25;
-	// x locations for the InfoBar
-	float bomb_text_x = 50;
-	float clock_text_x = 300;
-	float mistake_text_x = 550;
-	// font for Text objects
-	std::string font_file = "SourceSansPro.otf";
+
 	// initialize InfoBar
-	InfoBar m_info{ 0,bombs,0,info_text_yset,(float)win_width, font_file };
+	InfoBar m_info{ 0,bombs,0,info_text_yset,win_width, font_file };
 
 	// set/calculate GraphicCell dimensions
-	int b_width = 5;	// width of the border of a GraphicCell
+	int b_width = 5;							// width of the border of a GraphicCell
 	int c_width = win_width/dim - b_width*2;	// width of the interior of a GraphicCell
 	// initialize Board
-	Board m_board{ dim,bombs,c_width,b_width,(int)info_text_height };
-	m_board.print_board();		// compare graphic output with text-based version
+	int y_board_offset = (int)(info_bar_height + color_bar_height);
+	Board m_board{ dim,bombs,c_width,b_width,y_board_offset };
 
 	// initialize for use in window.isOpen() loop
 	Click user_action;
@@ -55,6 +60,10 @@ int main()
 	// to apply color shift effect in case of win
 	float elapsed = 0;			 // to keep track of time for color shift
 	float visual_interval = 250; // milliseconds between color shift 
+
+	//////////////////////////////////////////////////////////////////
+	// Play the game
+	//////////////////////////////////////////////////////////////////
 
 	while (window.isOpen())
 	{
@@ -99,7 +108,9 @@ int main()
 			}
 		}
 
+		// redraw the Board and ColorBar
 		m_board.draw(window);
+		m_color.draw(window, sf::RenderStates());
 
 		// update InfoBar if game is not yet won (update also draws to window)
 		if (!won)
